6eaa6cd99bd021034bb82aef692d8e6f7ec8de22
cyblair <cy@theblairs.us>
2023-11-01 16:07:59 -0400

    updated navbar and added cards

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar.js';
+import Card from './Card.js';
 
 function App() {
   return (
     <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home</a>
-            </li>
-          </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
-        <h1>CS 230L</h1>
-        <h2>Section 03</h2>
-        <p>WVU ID: 800400298</p>
-        <p>Hi, I am Cy</p>
+      <Navbar />
+      
+      <h1>CS 230L</h1>
+      <h2>Section 03</h2>
+      <p>WVU ID: 800400298</p>
+      <p>Hi, I am Cy</p>
+      <div>
+        <Card />
+      </div>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,25 @@
+function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">Home</a>
+                <a class="nav-item nav-link" href="#">About</a>
+                <a class="nav-item nav-link" href="#">Contact</a>
+                </div>
+            </div>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+  );
+}
+
+export default Navbar;
